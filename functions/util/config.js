0715f3680d58c0042d81d489f303e8fef1a99bf2
Jesús Cama <jesuscamaraymundo@gmail.com>
2021-03-20 23:36:51 -0500

    7 Refactoring & Organising

---
+++ functions/util/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyBVV8KlHpjeyu89WkqaFie0TrwjJ_EgkKc",
+  authDomain: "socialape-e3d91.firebaseapp.com",
+  projectId: "socialape-e3d91",
+  storageBucket: "socialape-e3d91.appspot.com",
+  messagingSenderId: "467743660202",
+  appId: "1:467743660202:web:0a678abf2574bf9a5418d3",
+};
